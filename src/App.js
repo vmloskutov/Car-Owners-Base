b5e033bf55e418b185e54aa7ae4b1f3d344d33cb
vmloskutov <vmloskutov@gmail.com>
2019-09-05 12:30:38 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import { Switch, Route, Link, Redirect } from "react-router-dom";
 import "./App.css";
 import Owners from './Components/Owners'
 
+function selectButton(e) {
+  let tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  e.currentTarget.className += " active";
+}
+
 const Comments = () => <div>You're on the Comments Tab</div>;
 const Contact = () => <div>You're on the Contact Tab</div>;
 class App extends Component {
   render() {
     return (
 
-      <div className="container">
+      <div className="container mt-3">
         <Switch>
           <Redirect from="/" to="/owners" />
         </Switch>
         <div className="tab">
-          <Link to={`/owners`} className="link"><button className="tablinks">Автовладельцы</button></Link>
-          <Link to={`/catalog`} className="link"><button className="tablinks">Каталог авто</button></Link>
-          <Link to={`/statistics`} className="link"><button className="tablinks">Статистика</button></Link>
+          <Link to={`/owners`} className="link"><button className="tablinks active" onClick={selectButton}>Автовладельцы</button></Link>
+          <Link to={`/catalog`} className="link"><button className="tablinks" onClick={selectButton}>Каталог авто</button></Link>
+          <Link to={`/statistics`} className="link"><button className="tablinks" onClick={selectButton}>Статистика</button></Link>
         </div>
         <div className="tabs">
           <Switch>
