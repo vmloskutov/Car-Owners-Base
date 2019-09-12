dd76eb7d1a183c9b864c013884c18a0e379c8f20
vmloskutov <vmloskutov@gmail.com>
2019-09-12 14:53:59 +0300

    fixed

--- src/Components/CurrentPerson.js
+++ src/Components/CurrentPerson.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Error from './Error'
 import Cars from './Cars'
 import Card from 'react-bootstrap/Card';
+var moment = require('moment');
+
 
 
 class CurrentPerson extends Component {
@@ -26,7 +28,7 @@ class CurrentPerson extends Component {
           </div>
           <br />
           <div className="autopark col-6">
-              {(this.props.data.cars.length !== 0) ? <Cars cars={this.props.data.cars} /> : <Error error="-" />}
+              {(this.props.data.cars.length !== 0) ? <Cars cars={this.props.data.cars} /> : (moment().diff(this.props.data.birthdate, 'years') < 18)? <Error error="Пользователю еще нет 18 лет" /> :<Error error="У этого пользователя нет автомобилей" />}
           </div>
         </div>
     )
